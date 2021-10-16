5438eccab371ed286928702ca35e5c8a1f9c2640
Rafael <rafael_breno@outlook.com>
2021-10-16 16:39:17 -0300

    feat: Add min rule

--- validator/rules/min.go
+++ validator/rules/min.go
@@ -3,82 +3,111 @@ package validator
 import "fmt"
 
 const (
-	fieldMin = `field lenght is less than %d`
+	fieldLengthMin = `field lenght is less than %d`
+	fieldValueMin  = `field is less than %d`
 )
 
 func Min(v interface{}, min int) string {
 	switch v.(type) {
+	case int:
+		if v.(int) < min {
+			return fmt.Sprintf(fieldValueMin, min)
+		}
+	case int64:
+		if v.(int64) < int64(min) {
+			return fmt.Sprintf(fieldValueMin, min)
+		}
+	case int32:
+		if v.(int32) < int32(min) {
+			return fmt.Sprintf(fieldValueMin, min)
+		}
+	case int16:
+		if v.(int16) < int16(min) {
+			return fmt.Sprintf(fieldValueMin, min)
+		}
+	case int8:
+		if v.(int8) < int8(min) {
+			return fmt.Sprintf(fieldValueMin, min)
+		}
+	case float64:
+		if v.(float64) < float64(min) {
+			return fmt.Sprintf(fieldValueMin, min)
+		}
+	case float32:
+		if v.(float32) < float32(min) {
+			return fmt.Sprintf(fieldValueMin, min)
+		}
 	case string:
 		if l := len(v.(string)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []string:
 		if l := len(v.([]string)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []int:
 		if l := len(v.([]int)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []int64:
 		if l := len(v.([]int64)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []int32:
 		if l := len(v.([]int32)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []int16:
 		if l := len(v.([]int16)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []int8:
 		if l := len(v.([]int8)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []uintptr:
 		if l := len(v.([]uintptr)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []uint:
 		if l := len(v.([]uint)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []uint64:
 		if l := len(v.([]uint64)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []uint32:
 		if l := len(v.([]uint32)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []uint16:
 		if l := len(v.([]uint16)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []uint8:
 		if l := len(v.([]uint8)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []float32:
 		if l := len(v.([]float32)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []float64:
 		if l := len(v.([]float64)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []complex64:
 		if l := len(v.([]complex64)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []complex128:
 		if l := len(v.([]complex128)); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	case []interface{}:
 		if l := len(v.([]interface{})); l < min {
-			return fmt.Sprintf(fieldMin, min)
+			return fmt.Sprintf(fieldLengthMin, min)
 		}
 	}
 	return ""
--- validator/rules/min_test.go
+++ validator/rules/min_test.go
@@ -14,7 +14,7 @@ func TestMin(t *testing.T) {
 		}
 	}
 	{
-		want := fmt.Sprintf(fieldMin, 7)
+		want := fmt.Sprintf(fieldLengthMin, 7)
 		got := Min("foo", 7)
 		if want != got {
 			t.Errorf(`want "%s", got "%s"`, want, got)
@@ -28,8 +28,22 @@ func TestMin(t *testing.T) {
 		}
 	}
 	{
-		want := fmt.Sprintf(fieldMin, 7)
+		want := fmt.Sprintf(fieldLengthMin, 7)
 		got := Min([]string{"foo", "bar"}, 7)
+		if want != got {
+			t.Errorf(`want "%s", got "%s"`, want, got)
+		}
+	}
+	{
+		want := ""
+		got := Min(20, 10)
+		if want != got {
+			t.Errorf(`want "%s", got "%s"`, want, got)
+		}
+	}
+	{
+		want := fmt.Sprintf(fieldValueMin, 10)
+		got := Min(7, 10)
 		if want != got {
 			t.Errorf(`want "%s", got "%s"`, want, got)
 		}
