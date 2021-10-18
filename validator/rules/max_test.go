f68583ce6d8b31def5a363a7359ad642f8926996
Rafael <rafael_breno@outlook.com>
2021-10-18 19:29:36 -0300

    fix: max rule

--- validator/rules/max_test.go
+++ validator/rules/max_test.go
@@ -8,42 +8,42 @@ import (
 func TestMax(t *testing.T) {
 	{
 		want := ""
-		got := Max("foo", 1)
+		got := Max("foo", 4)
 		if want != got {
 			t.Errorf(`want "%s", got "%s"`, want, got)
 		}
 	}
 	{
-		want := fmt.Sprintf(fieldLengthMax, 7)
-		got := Max("foo", 7)
+		want := fmt.Sprintf(fieldLengthMax, 2)
+		got := Max("foo", 2)
 		if want != got {
 			t.Errorf(`want "%s", got "%s"`, want, got)
 		}
 	}
 	{
 		want := ""
+		got := Max([]string{"foo", "bar"}, 3)
+		if want != got {
+			t.Errorf(`want "%s", got "%s"`, want, got)
+		}
+	}
+	{
+		want := fmt.Sprintf(fieldLengthMax, 1)
 		got := Max([]string{"foo", "bar"}, 1)
 		if want != got {
 			t.Errorf(`want "%s", got "%s"`, want, got)
 		}
 	}
 	{
-		want := fmt.Sprintf(fieldLengthMax, 7)
-		got := Max([]string{"foo", "bar"}, 7)
-		if want != got {
-			t.Errorf(`want "%s", got "%s"`, want, got)
-		}
-	}
-	{
 		want := ""
-		got := Max(20, 10)
+		got := Max(20, 30)
 		if want != got {
 			t.Errorf(`want "%s", got "%s"`, want, got)
 		}
 	}
 	{
 		want := fmt.Sprintf(fieldValueMax, 10)
-		got := Max(7, 10)
+		got := Max(20, 10)
 		if want != got {
 			t.Errorf(`want "%s", got "%s"`, want, got)
 		}
