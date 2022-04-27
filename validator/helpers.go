e3de05959f2e9739236079f4fd71cacd776ea5fb
Rafael <rafael_breno@outlook.com>
2022-04-26 22:52:48 -0300

    feat: add `ToPtr` function

--- validator/helpers.go
+++ validator/helpers.go
@@ -1,36 +1,7 @@
 package validator
 
-import (
-	"reflect"
-	"strings"
-	"unicode"
-)
-
-func isNil(i interface{}) bool {
-	if i == nil {
-		return true
-	}
-	switch reflect.TypeOf(i).Kind() {
-	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
-		return reflect.ValueOf(i).IsNil()
-	}
-	return false
-}
-
-func toLower(str string) string {
-	newStr := ""
-	firstUpper := true
-	for _, c := range str {
-		if unicode.IsUpper(c) && unicode.IsLetter(c) {
-			if firstUpper {
-				firstUpper = false
-				newStr = newStr + strings.ToLower(string(c))
-				continue
-			}
-			newStr = newStr + "_" + strings.ToLower(string(c))
-			continue
-		}
-		newStr = newStr + string(c)
-	}
-	return newStr
+// ToPtr converts any value
+// to a pointer.
+func ToPtr[T any](v T) *T {
+	return &v
 }
