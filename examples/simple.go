789bcaf162d895463d43c36d1e03984e7f546d28
Rafael <rafael_breno@outlook.com>
2022-04-26 22:52:01 -0300

    feat: add simple example of what should look like

--- examples/simple.go
+++ examples/simple.go
@@ -1,46 +1,60 @@
 package examples
 
-import (
-	"github.com/fiuskylab/go-valid/validator"
-	"github.com/fiuskylab/go-valid/validator/rules"
-)
+import "github.com/fiuskylab/go-valid/validator"
 
 type User struct {
-	Username             string `json:"username"`
-	Password             string `json:"password"`
-	PasswordConfirmation string `json:"password_confirmation"`
-	PhoneNumber          string `json:"phone_number"`
+	Username             string `v:"username"`
+	Password             string `v:"password"`
+	PasswordConfirmation string `v:"password_confirmation"`
+	Email                string `v:"email"`
 }
 
+const (
+	userRules = validator.Rules{
+		"username": {
+			Required: true,
+			Min:      6,
+			Max:      26,
+		},
+		"password": {
+			Required:  true,
+			Min:       8,
+			Max:       32,
+			Confirmed: true,
+		},
+		"email": {
+			Required: true,
+			Regex:    "some_regex",
+		},
+	}
+)
+
 func main() {
+	_ = validator.Rules{
+		"key": {
+			Min: validator.ToPtr[uint](1),
+		},
+	}
+
 	u := User{
-		Username:             "foo_bar",
-		Password:             "secret",
-		PasswordConfirmation: "secret",
-		PhoneNumber:          "",
+		Username:             "rafiusky",
+		Password:             "something",
+		PasswordConfirmation: "something",
+		Email:                "rafiusky@email.com",
 	}
 
-	rules := rules.Rules{
-		"username": rules.Rule{
-			Required: true,
-			Min:      4,
-			Max:      16,
-		},
-		"password": rules.Rule{
-			Required: true,
-			Min:      8,
-			Max:      32,
-		},
-		"passwordconfirmation": rules.Rule{
-			Required: true,
-			Min:      8,
-			Max:      32,
-		},
-		"phonenumber": rules.Rule{
-			Required: false, // Default
-			Regex:    `/some_regex/`,
-		},
-	}
+	// Here the error is for internal errors,
+	// not validation ones.
+	res, err := userRules.Validate(u)
 
-	validator.Check(u, rules)
+	// Returns a boolean, if the
+	// struct is valid or not
+	if !res.Valid() {
+		// handles if not valid
+
+		// MapResults returns the validation errors
+		// in the format of a map, so it can be returned
+		// in a JSON format.
+		m := res.MapResults()
+	}
 }
